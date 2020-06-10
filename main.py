6ecfad1e88cb83ab0b8dbf97571db1cbd7584c0a
CamClrt <camille.clarret@gmail.com>
2020-06-10 15:30:23 +0200

    init main

---
+++ main.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+if __name__ == "__main__":
+    pass
+
+    #if doesn't exist, init DB
+    #insert values in DB
+    #connect to DB & display report
---
+++ src/main.py
@@ -0,0 +1,14 @@
+"""
+    This application is made for the project 6 on OpenClassrooms during the
+    Python courses
+    The aim of this project is to create and test a MySQL database
+"""
+
+#! /usr/bin/env python3
+# coding: utf-8
+
+if __name__ == "__main__":
+    pass
+
+    #if DB doesn't exist, init it + insert values
+    #connect to DB and display report
