12a09aa8ba367ed3fa5be14203c5db1e373ec247
CamClrt <camille.clarret@gmail.com>
2020-06-23 11:36:14 +0200

    test(dazta insertion): prepare a set of data + test folder

---
+++ data.py
@@ -0,0 +1,120 @@
+ACCESS = [
+    "Créer",
+    "Lire",
+    "Mettre à jour",
+    "Supprimer",
+]
+
+ACCOUNT_TYPE = [
+    "Administrateur",
+    "Fondateur",
+    "Responsable",
+    "Pizzaïolo",
+    "Livreur",
+    "Client",
+]
+
+STATUS = [
+    "en attente de préparation",
+    "en préparation",
+    "en attente de remise",
+    "remise",
+    "en attente de livraison",
+    "en livraison",
+    "livrée",
+    "incident",
+]
+
+PAYMENT_METHOD = [
+    "espèces",
+    "chèque",
+    "titre restaurant",
+    "carte bancaire",
+]
+
+ESTABLISHMENT = [
+    "Pizzeria n°1",
+    "Pizzeria n°2",
+    "Pizzeria n°3",
+    "Pizzeria n°4",
+    "Pizzeria n°5",
+]
+
+INIT = [
+    {"pièce": "pièce"},
+    {"pincée": "pincée"},
+    {"grammes": "g"},
+    {"kilogrammes": "kg"},
+    {"litre": "L"},
+    {"décilitre": "dL"},
+    {"centilitre": "cL"},
+    {"millilitre": "mL"},
+]
+
+PRODUCT = [
+    "MARGHERITA",
+    "BURRATA FELICITÀ",
+    "HOT AND CHEESY",
+    "REGINA 4 EVER",
+    "IN LOVE WITH ROME",
+    "CARBOLOVALOVA",
+    "STRACIABELLA",
+    "CHAMP-MIGNON",
+    "REGINA INSTAGRAM",
+    "MOMENT OF TRUFFE",
+    "HOTLINE PIZZ",
+    "NOTORIOUS B.I.CHEESE",
+]
+
+CATEGORY = [
+    "LES BEST SELLERS",
+    "LES INCONTOURNABLES",
+    "LES VEGETARIENNES",
+    "LES VEGANNES",
+    "LES CARNIVORES",
+    "LES NOUVEAUTES",
+]
+
+INGREDIENT = [
+    "pecorino",
+    "guanciale",
+    "avocat",
+    "basilic frais",
+    "straccitella",
+    "sucrine",
+    "radis",
+    "sauce tomate",
+    "mozza fior di latte",
+    "champignons",
+    "sauce de tomates San Marzano",
+    "tomates cerises confites",
+    "olives taggiasche",
+    "oignons",
+    "huile d’olive à l’ail",
+    "courgettes marinées",
+    "tomates datterino confites",
+    "aiguillettes de poulet mariné",
+    "ricotta aux herbes",
+    "cœur de burrata",
+    "sésame",
+    "tomates San Marzano",
+    "basilic",
+    "orange caramélisée",
+    "spianata piccante",
+    "jambon cuit aux herbes",
+    "salsa ceasar",
+    "chou fleur",
+    "vinaigrette au balsamique",
+    "cresson",
+    "pasto della casa",
+    "petites tomates jaunes confites",
+    "parmegiano",
+    "crème de gorgonzola",
+    "mozzarella",
+    "prosciutto",
+    "pain croquant",
+    "pousses d’épinard",
+    "chou kale",
+    "brocoli",
+    "pignons",
+]
